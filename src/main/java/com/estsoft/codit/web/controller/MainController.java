b5479f87f61b7ce8deada5636efc2b8f4fc7f893
Jisunglim <iejisung@gmail.com>
2016-06-21 18:02:58 +0900

    make a big deal

--- src/main/java/com/estsoft/codit/web/controller/MainController.java
+++ src/main/java/com/estsoft/codit/web/controller/MainController.java
@@ -1,7 +1,16 @@
 package com.estsoft.codit.web.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by JisungLim on 2016-06-20.
  */
+@Controller
 public class MainController {
+
+  @RequestMapping("/")
+  public String index() {
+    return "";
+  }
 }
