ae3ca09027590e17f6c681ace97b2ef191059a32
Jisung Lim <iejisung@gmail.com>
2016-06-23 14:05:31 +0900

    recruit ing

--- src/main/java/com/estsoft/codit/web/controller/RecruitController.java
+++ src/main/java/com/estsoft/codit/web/controller/RecruitController.java
@@ -1,11 +1,27 @@
 package com.estsoft.codit.web.controller;
 
+import com.estsoft.codit.web.service.RecruitService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
  * Created by JisungLim on 2016-06-20.
  */
 @Controller
+@RequestMapping("/recruit")
 public class RecruitController {
+
+  @Autowired
+  RecruitService recruitService;
+
+//  @RequestMapping("/{id}")
+//  public String main(@PathVariable("id") int id) {
+//
+//  }
+
+
 }
--- src/main/java/com/estsoft/codit/web/service/RecruitService.java
+++ src/main/java/com/estsoft/codit/web/service/RecruitService.java
@@ -1,7 +1,11 @@
 package com.estsoft.codit.web.service;
 
+import org.springframework.stereotype.Service;
+
 /**
  * Created by JisungLim on 2016-06-21.
  */
+
+@Service
 public class RecruitService {
 }
