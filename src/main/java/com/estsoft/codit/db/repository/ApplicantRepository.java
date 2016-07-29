9abe603d92f206600e0b85acc0999d8962229cc1
joonho-yeom <joonhoyeom@gmail.com>
2016-07-29 16:39:47 +0900

    logging library 제거

--- src/main/java/com/estsoft/codit/db/repository/ApplicantRepository.java
+++ src/main/java/com/estsoft/codit/db/repository/ApplicantRepository.java
@@ -1,15 +1,11 @@
 package com.estsoft.codit.db.repository;
 
 import com.estsoft.codit.db.vo.ApplicantVo;
-
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Repository
 public class ApplicantRepository {
